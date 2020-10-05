62055a9d5e81404fb8063e80a27d587344f49b02
Nemanja Tosic <netosic90@gmail.com>
2020-10-05 10:28:41 +0200

    chore: export additional types from slate-collaborative-client
    Prevents the need to import from /lib/ directly, and importing both
    client and bridge.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,16 @@
 import useCursor from './useCursor'
 import withAutomerge from './withAutomerge'
-import withSocketIO from './withSocketIO'
+import withSocketIO, { WithSocketIOEditor } from './withSocketIO'
 import withIOCollaboration from './withIOCollaboration'
+import { AutomergeEditor } from './automerge-editor'
+import { CursorData } from '@livingspec/slate-collaborative-bridge'
 
-export { withAutomerge, withSocketIO, withIOCollaboration, useCursor }
+export {
+  withAutomerge,
+  withSocketIO,
+  withIOCollaboration,
+  useCursor,
+  WithSocketIOEditor,
+  AutomergeEditor,
+  CursorData
+}
